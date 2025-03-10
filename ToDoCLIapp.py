aebea1a4f4ac0ad63984cbc20200f94c10445718
Jakub40480 <Jakub40480@gmail.com>
2025-03-10 10:43:46 +0100

    Completed add function

--- ToDoCLIapp.py
+++ ToDoCLIapp.py
@@ -3,7 +3,7 @@ import json
 import random
 import os
 
-ids = random.randint(0, 1000)
+IDs = random.randint(0, 1000)
 data_file = "data.json"
 
 
@@ -12,20 +12,36 @@ def add_task():
     task_description = input("Description(optional): ")
     time_created = time.ctime()
     time_updated = time.ctime()
-    unique_id = ids
+    unique_id = IDs
     used_ids = []
     if unique_id in used_ids:
-        unique_id = ids
+        unique_id = IDs
     else:
         used_ids.append(unique_id)
-    task_status  = input("todo(1), in-progress(2), done(3)")
+    while True:
+        try:
+            task_status = int(input("todo(1), in-progress(2), done(3): "))
+        except ValueError:
+            pass
+        if task_status not in range(1, 4):
+            print("Invalid input for task status. Please enter 1, 2, or 3.") 
+        else:
+            task_status = str(task_status)
+            if task_status == "1":
+                task_status == "todo"
+            elif task_status == "2":
+                task_status == "in-progress"
+            else:
+                task_status = "done"
+            break
+
     task = {
         "ID": unique_id,
         "name": task_name,
         "description": task_description, 
         "time created": time_created,
         "time updated": time_updated,
-        "task_status": task_status
+        "status": task_status
     }
     if not os.path.exists(data_file):
         with open(data_file, "w") as file:
